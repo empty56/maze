565b8d909450de72d5e65c260b3455619c4ef691
dmmaks <dm.maksymenko@gmail.com>
2022-11-13 14:29:26 +0200

    added stars on level screen

--- Assets/Scripts/LevelsMenu.cs
+++ Assets/Scripts/LevelsMenu.cs
@@ -1,10 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class LevelsMenu : MonoBehaviour
 {
+    public TextMeshProUGUI level1score;
+    public Image level1stars;
+    public Sprite oneStarSprite;
+    public Sprite twoStarsSprite;
+    public Sprite threeStarsSprite;
+
+    void Start () {
+        if (PlayerPrefs.HasKey("bestTime1") && PlayerPrefs.HasKey("bestStars1")) {
+            float bestTime1 = PlayerPrefs.GetFloat("bestTime1");
+            float bestStars1 = PlayerPrefs.GetInt("bestStars1");
+            level1score.text = "HIGH SCORE: " + bestTime1.ToString("F2");
+            if (bestStars1 == 1) {
+                level1stars.sprite = oneStarSprite;
+            } else if (bestStars1 == 2) {
+                level1stars.sprite = twoStarsSprite;
+            } else {
+                level1stars.sprite = threeStarsSprite;
+            }
+        }
+	}
+
     public void StartLevel1() {
         SceneManager.LoadScene("Level_1");
     }
