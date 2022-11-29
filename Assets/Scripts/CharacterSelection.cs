04c93ab67373db3e314be633ff4922d3d59903c8
Verbovyi Dmytro <dima.verbowoy@gmail.com>
2022-11-29 23:42:14 +0200

    choose character
    Started character selection development

---
+++ Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterSelection : MonoBehaviour
+{
+public int selectedCharacter = 0;
+public GameObject[] characters;
+
+    public void NextCharacte()
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter = (selectedCharacter + 1) % characters.Length;
+        characters[selectedCharacter].SetActive(true);
+    }
+
+    public void PreviousCharacter()
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if (selectedCharacter < 0)
+        {
+            selectedCharacter += characters.Length;
+            characters[selectedCharacter].SetActive(true);
+        }
+    }
+
+    public void SelectCharacter()
+    {
+        PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
+    }
+}
